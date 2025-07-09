74f83feda724b884c1487da5f96ab39204baea97
keerthisharan7 <48143812+keerthisharan7@users.noreply.github.com>
2025-07-09 11:27:24 +0530

    Say Hi - Added code.

---
+++ sayhijs.js
@@ -0,0 +1,31 @@
+const contactForm = document.querySelector('.contact-form');
+if (contactForm) {
+  contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Message sent! I will get back to you soon.');
+    contactForm.reset();
+  });
+}
+
+const nameInput = document.querySelector('.contact-form input[type="text"]');
+
+nameInput.addEventListener('input', () => {
+  const nameValue = nameInput.value.trim();
+
+  const isValid = /^[A-Za-z]+(?: [A-Za-z]+)*$/.test(nameValue) && nameValue.length >= 3;
+
+  // Remove red if valid
+  if (isValid || nameValue === "") {
+    nameInput.classList.remove('invalid');
+  } 
+  // Only trigger shake when it just became invalid
+  else if (!nameInput.classList.contains('invalid')) {
+    nameInput.classList.add('invalid');
+
+    // Trigger shake once
+    nameInput.classList.add('shake');
+    setTimeout(() => {
+      nameInput.classList.remove('shake');
+    }, 400);
+  }
+});
